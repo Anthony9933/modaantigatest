a2406b0b49c5935798e7842cab5b1697adbcfa51
Anthony <86032299+Anthony9933@users.noreply.github.com>
2024-05-23 09:04:43 -0300

    Update code.py

--- code.py
+++ code.py
@@ -2,6 +2,9 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
+# Carregar os dados
+df = pd.read_csv('dados.csv')
+
 # Configurar o layout da página
 st.set_page_config(layout="wide")
 
@@ -40,7 +43,7 @@ def show_overview():
 def show_filters_data():
     # Título da aplicação
     st.title('Análise de Dados da Loja de Roupas Femininas')
-    df = pd.read_csv('dados.csv')
+    
     # Mostrar os dados
     st.header('Dados Brutos')
     st.write(df)
@@ -63,11 +66,11 @@ def show_filters_data():
     
     # Gráfico de evolução das vendas ao longo do tempo
     st.header('Evolução das Vendas ao Longo do Tempo')
-    df['Data'] = pd.to_datetime(df['Data'], errors='coerce')
-    df = df.dropna(subset=['Data'])
-    df['AnoMes'] = df['Data'].dt.to_period('M')
-    vendas_mensais = df.groupby('AnoMes').sum().reset_index()
-    fig4 = px.line(vendas_mensais, x='AnoMes', y='PdVenda', title='Evolução das Vendas ao Longo do Tempo', labels={'AnoMes': 'Data', 'PdVenda': 'Total de Vendas'})
+    # Converter a coluna 'Data' para datetime
+    df['Data'] = pd.to_datetime(df['Data'], format='%Y-%m')
+    # Agrupar por ano e mês
+    vendas_mensais = df.groupby(df['Data'].dt.to_period('M')).sum().reset_index()
+    fig4 = px.line(vendas_mensais, x='Data', y='PdVenda', title='Evolução das Vendas ao Longo do Tempo', labels={'Data': 'Data', 'PdVenda': 'Total de Vendas'})
     st.plotly_chart(fig4)
     
     # Gráfico de relação entre quantidade e preço de custo
