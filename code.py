813534c7a445547db2b7e3620ef73193616d41bf
Anthony <86032299+Anthony9933@users.noreply.github.com>
2024-05-30 14:48:20 -0300

    Update code.py

--- code.py
+++ code.py
@@ -9,7 +9,7 @@ page = st.sidebar.selectbox("Escolha a Página", ["Visão Geral", "Filtros e Dad
 @st.cache_data
 def load_data():
     # Substitua 'sua_base_de_dados_ajustada.csv' pelo caminho para o seu arquivo de dados
-    df = pd.read_csv('adjusted_data2.csv')
+    df = pd.read_csv('adjusted_data4.csv')
     df['Data'] = pd.to_datetime(df['Data'], format='%Y-%m-%d')
     return df
 
